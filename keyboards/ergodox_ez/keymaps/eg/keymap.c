73b17bb8d38a53f68d9505d806886ded0a8ac2a6
eg-itn <brokmn@gmail.com>
2026-06-16 15:00:04 +0900

    change LT(2,ESC) to MO(2)

--- keyboards/ergodox_ez/keymaps/eg/keymap.c
+++ keyboards/ergodox_ez/keymaps/eg/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
  * | Del    |   Q  |   W  |   E  |   R  |   T  | MO(1)|           | MO(1)|   Y  |   U  |   I  |   O  |   P  |  Enter |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | 2|ESC  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |  '     |
+ * | MO(2)  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |  '     |
  * |--------+------+------+------+------+------| MO(1)|           | MO(1)|------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  | SLSH | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // left hand
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LEFT, KC_RGHT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    MO(1),   MO(1),   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
-    LT(2,KC_ESC), KC_A, KC_S,  KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    MO(2),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    MO(1),   MO(1),   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
     KC_LCTL, KC_LGUI, KC_LALT, KC_LALT, MO(3),                                       MO(3),   MO(4),   MO(1),   KC_APP,  KC_RCTL,
                                         KC_SPC,  KC_SPC,                     KC_LALT, KC_SPC,
